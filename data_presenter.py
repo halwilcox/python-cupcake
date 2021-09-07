9bff6b09ecdea63133eea3f3363c209157bf1d8b
halwilcox <halwilcox09@gmail.com>
2021-09-07 16:50:23 -0600

    python

--- data_presenter.py
+++ data_presenter.py
@@ -1,9 +1,19 @@
 open_file = open("CupcakeInvoices.csv")
 
+sum = 0
+
+total_sales =[]
 for line in open_file:
     line = line.rstrip('\n').split(",")
-    print(line)
+    print (line)
+    print(line[2])
+    quantity = float(line[3])
+    price = float(line[4])
+    print(quantity * price)
+   # total_sales.append(quantity * price)
+    sum += (quantity * price)
 
-#for line in open_file:
- #   line= line.rstrip('\n').split(",")
-  #  print(line[2])
+
+#done = sum(total_sales)
+print(sum)
+#Float-method
--- starter.py
+++ starter.py
@@ -42,9 +42,9 @@ for numbers in numbers:
 # Create an empty array called `even_numbers`.
 # Use a for-in loop to iterate over the `numbers` array, and if a number is even, add  it to the `even_numbers` array.
 even_numbers = []
-def extract_even(numbers)
-for numbers in numbers:
-    if number % 2: 
+#def extract_even(numbers)
+#for numbers in numbers:
+ #   if number % 2: 
      
 
 # Problem 10
@@ -55,31 +55,27 @@ score = 74
 # Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console-log an 'A', between 80 and 89, console-log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console-log an 'F'.
 
 if score >= 90:
-    print:("A")
-elif score 89 >= 80:
-    print:("B")
-elif score 79 >= 70:
-    print:("C")
-elif score < 60:
-    print:("F")
+    print("A")
+elif 80 <= 89:
+    print("B")
+elif 70 <= score < 80:
+    print("C")
+elif 60 <= score < 70:
+    print("D")
+else:
+    print("F")
 
 
 
 # Problem 11
 # Create a variable called 'changeMyMind' and set it equal to true. 
 # Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
-Change_My_Mind = False
+changeMyMind = True
 
-def boolean():
-    if Change_My_Mind == True:
-        Change_My_Mind = False
-    else: 
-    Change_My_Mind = True
-    
-
-
-    print(Change_My_Mind)
-
+if changeMyMind == True:
+    print(False)
+else:
+    print(True)
 
 # ADVANCED
 
